6018aa66abadd213867f9b1b1ef4b6db8e791eb1
Ced2911 <Ced2911@gmail.com>
2012-09-21 21:52:58 +0200

    tmp fix until i found something better

--- source/winput.c
+++ source/winput.c
@@ -22,6 +22,22 @@
 #include "video.h"
 #include "input.h"
 
+#include <stdint.h>
+
+struct controller_data_s
+{
+	signed short s1_x, s1_y, s2_x, s2_y;
+	int s1_z, s2_z, lb, rb, start, back, a, b, x, y, up, down, left, right;
+	unsigned char lt, rt;
+	int logo;
+};
+
+int get_controller_data(struct controller_data_s *d, int port);
+
+void set_controller_data(int port, const struct controller_data_s *d);
+
+void set_controller_rumble(int port, uint8_t l, uint8_t r);
+
 void doScreenCapture();
 
 #define MAX_INPUTS 4
